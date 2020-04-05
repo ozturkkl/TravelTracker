2ed4f83e2a50277ea1a7246297132e06c1080fcd
manchava <manchava@mail.uc.edu>
2020-04-04 23:56:54 -0400

    Updated pages for app, added constraints for existing features, and added data classes

--- app/src/main/java/uc/edu/ozturkkl/traveltracker/dto/Places.kt
+++ app/src/main/java/uc/edu/ozturkkl/traveltracker/dto/Places.kt
@@ -5,3 +5,21 @@ data class Places(var Country: String = "", var Journal:String = "", var Rating:
         return "$Country $Journal $State"
     }
 }
+
+data class Restaurants(var Name: String = "", var Address: String = "", var Rating: Int = 0) {
+    override fun toString(): String {
+        return "$Name $Address $Rating"
+    }
+}
+
+data class Entertainments(var Name: String = "", var Address: String = "", var Age: String = "", var Rating: Int = 0){
+    override fun toString(): String {
+        return "$Name $Address $Rating"
+    }
+}
+
+data class Gas(var Name: String = "", var Address: String = "", var Price: Double = 0.0, var PaymentType: String = "" ) {
+    override fun toString(): String {
+        return "$Name $Address, $Price, $PaymentType"
+    }
+}
