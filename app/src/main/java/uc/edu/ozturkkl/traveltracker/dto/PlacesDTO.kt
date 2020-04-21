a024110c68b8b4b30ac0556a03612719b61b9d49
Kemal Ozturk <97kemalozturk@gmail.com>
2020-04-21 03:06:59 -0400

    Added buttons and screen transactions

--- app/src/main/java/uc/edu/ozturkkl/traveltracker/dto/PlacesDTO.kt
+++ app/src/main/java/uc/edu/ozturkkl/traveltracker/dto/PlacesDTO.kt
@@ -1,6 +1,6 @@
 package uc.edu.ozturkkl.traveltracker.dto
 
-class PlacesDTO(var location: String = "", var description:String = "", var rating:Int = 0) {
+class  PlacesDTO(var location: String = "", var description:String = "", var rating:Int = 0) {
     override fun toString(): String {
         return "$location $description $rating"
     }
