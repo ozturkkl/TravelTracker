dc14db899b9ad12f901ec688ba7200494441504f
Kemal Ozturk <97kemalozturk@gmail.com>
2020-04-24 02:20:52 -0400

    Added locations display functionality to BeenTo screen

--- app/src/main/java/uc/edu/ozturkkl/traveltracker/ui/beenTo/BeenToFragment.kt
+++ app/src/main/java/uc/edu/ozturkkl/traveltracker/ui/beenTo/BeenToFragment.kt
@@ -1,9 +1,12 @@
 package uc.edu.ozturkkl.traveltracker.ui.beenTo
 
+import android.content.Context
+import android.media.Rating
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -11,11 +14,15 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.been_to_fragment.*
+import kotlinx.android.synthetic.main.location_template.*
 import uc.edu.ozturkkl.traveltracker.R
+import uc.edu.ozturkkl.traveltracker.dto.LocationDTO
 import uc.edu.ozturkkl.traveltracker.ui.main.MainViewModel
 
 
 class BeenToFragment : Fragment() {
+
+    private lateinit var locations:ArrayList<LocationDTO>
 
     companion object {
         fun newInstance() = BeenToFragment()
@@ -30,63 +37,53 @@ class BeenToFragment : Fragment() {
         return inflater.inflate(R.layout.been_to_fragment, container, false)
         }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         if(context != null){
 
             viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
             viewModel.locations.observe(this, androidx.lifecycle.Observer{
                     locations ->
-                locations.forEach{
-                    val card = CardView(context!!)
+                this.locations = locations
+                beenToList.adapter = CustomAdapter(beenToList.context ,locations)
 
-                    card.layoutParams =
-                        ConstraintLayout
-                            .LayoutParams(
-                                ConstraintLayout.LayoutParams.MATCH_PARENT,
-                                ConstraintLayout.LayoutParams.WRAP_CONTENT
-                            )
-
-                    val locationTextView = TextView(context!!)
-                    locationTextView.layoutParams =
-                        ConstraintLayout
-                            .LayoutParams(
-                                ConstraintLayout.LayoutParams.MATCH_PARENT,
-                                ConstraintLayout.LayoutParams.WRAP_CONTENT
-                            )
-                    locationTextView.text = it.name
-
-                    val ratingBar = RatingBar(context!!)
-                    locationTextView.layoutParams =
-                        ConstraintLayout
-                            .LayoutParams(
-                                ConstraintLayout.LayoutParams.MATCH_PARENT,
-                                ConstraintLayout.LayoutParams.WRAP_CONTENT
-                            )
-                    ratingBar.numStars = 5
-                    ratingBar.rating = it.rating
-
-                    val descriptionTextView = TextView(context!!)
-                    locationTextView.layoutParams =
-                        ConstraintLayout
-                            .LayoutParams(
-                                ConstraintLayout.LayoutParams.MATCH_PARENT,
-                                ConstraintLayout.LayoutParams.WRAP_CONTENT
-                            )
-                    descriptionTextView.text = it.description
-
-                    card.addView(locationTextView)
-                    card.addView(ratingBar)
-                    card.addView(descriptionTextView)
-                    card_layout.addView(card)
-                }
             })
+        }
+
+    }
+
+    private class CustomAdapter(context: Context?, locations: ArrayList<LocationDTO>): BaseAdapter() {
+        private val locations: ArrayList<LocationDTO> = locations
+        private val context: Context? = context
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val inflater = LayoutInflater.from(context)
+            val locationTemplate = inflater.inflate(R.layout.location_template, parent, false)
+
+            val location = locationTemplate.findViewById<TextView>(R.id.lblLocation)
+            location.text = locations[position].name
+
+            val description = locationTemplate.findViewById<TextView>(R.id.txtDescription)
+            description.text = locations[position].description
+
+            val rating = locationTemplate.findViewById<RatingBar>(R.id.ratingLocation)
+            rating.rating = locations[position].rating
+
+            return locationTemplate
+        }
+
+        override fun getItem(position: Int): Any {
+            return "test string"
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return locations.size
         }
 
     }
